21bcf6aa2b1077202f4d533d88b142b15b97cd7e
Florin Postolache <florinpostolache@192-168-0-107.rdsnet.ro>
2024-01-07 00:08:08 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<script setup>
+import { ref } from "vue";
+import { StarIcon } from "@heroicons/vue/24/solid";
+import { items } from "./movies.json";
+const movies = ref(items);
+
+function updateRating(movieIndex, rating) {
+  movies.value[movieIndex].rating = rating;
+}
+</script>
+
+<template>
+  <div class="app">
+    <div class="movie-list">
+      <div
+        class="movie-item"
+        v-for="(movie, movieIndex) in movies"
+        :key="movie.id"
+      >
+        <div class="movie-item-image-wrapper relative">
+          <div class="absolute top-0 right-0 w-10 h-10">
+            <div class="relative">
+              <StarIcon class="text-yellow-500" />
+              <div class="absolute top-1/2 right-1/2  translate-x-1/2 -translate-y-1/2">{{ movie.rating ?? '-' }}</div>
+            </div>
+          </div>
+          <img :src="movie.image" class="movie-item-image" alt="" />
+        </div>
+
+        <div class="movie-item-content-wrapper">
+          <div class="movie-item-title-wrapper">
+            <h3 class="movie-item-title">{{ movie.name }}</h3>
+            <div class="movie-item-genres-wrapper">
+              <span
+                v-for="genre in movie.genres"
+                :key="`${movie.id}-${genre}`"
+                class="movie-item-genre-tag"
+                >{{ genre }}</span
+              >
+            </div>
+          </div>
+          <div class="movie-item-description-wrapper">
+            <p class="movie-item-description">{{ movie.description }}</p>
+          </div>
+          <div class="movie-item-rating-wrapper">
+            <span class="movie-item-rating-text">
+              Rating: ({{ movie.rating }}/5)
+            </span>
+
+            <div class="movie-item-star-icon-wrapper">
+              <button
+                v-for="star in 5"
+                :key="star"
+                class="movie-item-star-icon-button"
+                :class="[
+                  star <= movie.rating ? 'text-yellow-500' : 'text-gray-500',
+                ]"
+                :disabled="star === movie.rating"
+                @click="updateRating(movieIndex, star)"
+              >
+                <StarIcon class="movie-item-star-icon" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
